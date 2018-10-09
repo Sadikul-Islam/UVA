251d070f2017c21e391986e126fc8cd5078374d4
Sadikul-Islam <netshuvo@gmail.com>
2018-10-09 23:13:19 +0600

    Added 10970 - Big Chocolate.cpp

---
+++ 10970 - Big Chocolate.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int m,n,a,b;
+    while(cin>>m>>n)
+    {
+        a=m*n;
+        b=a-1;
+        cout<<b<<endl;
+    }
+
+    return 0;
+}
